19e4fc232571bf2c980e37bcc83b2ff24ef09a7e
skris92 <szurcsik92@gmail.com>
2022-03-26 12:47:10 +0100

    Ant abstract class

---
+++ src/Codecool.LifeOfAnts/Ant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.LifeOfAnts
+{
+    abstract class Ant
+    {
+        public Position Position;
+        protected Ant(int x, int y)
+        {
+            Position = new Position(x, y);
+        }
+        virtual public void Act(Ant[] area) { }
+    }
+}
