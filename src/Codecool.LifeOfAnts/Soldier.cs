36bcd4fdc852ef3298c5331bbf70fdf6cdb47551
skris92 <szurcsik92@gmail.com>
2022-03-26 22:36:11 +0100

    Fully functioning Act method

--- src/Codecool.LifeOfAnts/Soldier.cs
+++ src/Codecool.LifeOfAnts/Soldier.cs
@@ -17,7 +17,46 @@ namespace Codecool.LifeOfAnts
         public override void Act(Ant[,] area)
         {
             List<Position> AntPositions = GetAntPositionsList(area);
-
+            Position NextPosition = GenerateNextPosition(area);
+            if (!IsPositionOccupied(AntPositions, NextPosition))
+            {
+                Position = new Position(NextPosition.X, NextPosition.Y);
+            }
+        }
+        private Position GenerateNextPosition(Ant[,] area)
+        {
+            switch (facing)
+            {
+                case Direction.North:
+                    facing += 1;
+                    if (this.Position.X > 0)
+                    {
+                        return new Position(this.Position.X - 1, this.Position.Y);
+                    }
+                    break;
+                case Direction.South:
+                    facing += 1;
+                    if (this.Position.X < area.GetLength(1) - 1)
+                    {
+                        return new Position(this.Position.X + 1, this.Position.Y);
+                    }
+                    break;
+                case Direction.West:
+                    facing += 1;
+                    if (this.Position.Y > 0)
+                    {
+                        return new Position(this.Position.X, this.Position.Y - 1);
+                    }
+                    break;
+                case Direction.East:
+                    facing = Direction.North;
+                    if (this.Position.Y < area.GetLength(0) - 1)
+                    {
+                        return new Position(this.Position.X, this.Position.Y + 1);
+                    }
+                    break;
+            }
+            return new Position(this.Position.X, this.Position.Y);
         }
     }
 }
