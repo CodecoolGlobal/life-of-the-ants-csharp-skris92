be11a6f599b10dc9caee5394236ad9b41b38adc2
skris92 <szurcsik92@gmail.com>
2022-03-26 14:16:50 +0100

    Colony class with skeleton

---
+++ src/Codecool.LifeOfAnts/Colony.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.LifeOfAnts
+{
+    public class Colony
+    {
+        private Ant[,] area;
+        public Colony(int size)
+        {
+            area = new Ant[size, size];
+            area[size / 2, size / 2] = new Queen(size / 2, size / 2);
+        }
+        public void Display()
+        {
+
+        }
+        public void GenerateAnts(int drone, int soldier, int worker)
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
