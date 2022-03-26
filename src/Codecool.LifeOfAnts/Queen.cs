68e0e8f17f47d24b0d55ba3ba2c1426897ba3cd4
skris92 <szurcsik92@gmail.com>
2022-03-26 13:13:10 +0100

    Queen class

---
+++ src/Codecool.LifeOfAnts/Queen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.LifeOfAnts
+{
+    public class Queen : Ant
+    {
+        private bool isInMood;
+        private int moodCooldown;
+        public bool IsInMood => isInMood;
+        public Queen(int x, int y) : base(x, y)
+        {
+            isInMood = true;
+            moodCooldown = 0;
+        }
+        public void Mate()
+        {
+            isInMood = false;
+            Random random = new Random();
+            moodCooldown = random.Next(50,101);
+        }
+        public override void Act()
+        {
+            if (moodCooldown != 0) moodCooldown--;
+            else if (moodCooldown == 0 && isInMood == false) isInMood = true;
+        }
+    }
+}
