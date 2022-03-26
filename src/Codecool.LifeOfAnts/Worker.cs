ff21b160905705a65a437d510e3dd2ec08cd99be
skris92 <szurcsik92@gmail.com>
2022-03-26 14:25:23 +0100

    Worker class with skeleton

---
+++ src/Codecool.LifeOfAnts/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.LifeOfAnts
+{
+    public class Worker : Ant
+    {
+        public Worker(int x, int y) : base (x, y)
+        {
+
+        }
+        public override void Act()
+        {
+            Random random = new Random();
+            Direction randomDrection = (Direction)random.Next(0, 5);
+        }
+    }
+}
