05b14c779f74288134b577b791096d173c78c0c1
skris92 <szurcsik92@gmail.com>
2022-03-26 14:20:21 +0100

    Soldier class with skeleton and constructor

---
+++ src/Codecool.LifeOfAnts/Soldier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecool.LifeOfAnts
+{
+    public class Soldier : Ant
+    {
+        private Direction facing;
+        public Soldier(int x, int y) : base (x, y)
+        {
+            Random random = new Random();
+            facing = (Direction)random.Next(0,5);
+        }
+        public override void Act(Ant[,] area)
+        {
+            
+        }
+    }
+}
