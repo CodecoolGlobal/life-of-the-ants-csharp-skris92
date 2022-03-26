4a005fe160f07ebf59486daeb64ad83ea08ddf8c
skris92 <szurcsik92@gmail.com>
2022-03-26 15:30:01 +0100

    Central queen construction fix

--- src/Codecool.LifeOfAnts/Colony.cs
+++ src/Codecool.LifeOfAnts/Colony.cs
@@ -12,12 +12,20 @@ namespace Codecool.LifeOfAnts
         public Colony(int size)
         {
             area = new Ant[size, size];
-            int center = Convert.ToInt32(Math.Round(Convert.ToDouble(size) / 2));
-            area[center, center] = new Queen(center, center);
+            int centerIndex = (size % 2 == 0) ? size / 2 - 1 : size / 2;
+            area[centerIndex, centerIndex] = new Queen(centerIndex, centerIndex);
         }
         public void Display()
         {
-
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                for (int j = 0; j < area.GetLength(1); j++)
+                {
+                    if (area[i, j] == null) Console.Write("- ");
+                    else Console.Write($"{area[i, j].ToString()} ");
+                    if (j == area.GetLength(0) - 1) Console.WriteLine();
+                }
+            }
         }
         public void GenerateAnts(int drone, int soldier, int worker)
         {
