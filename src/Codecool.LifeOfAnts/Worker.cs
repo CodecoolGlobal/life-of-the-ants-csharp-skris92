aa47370e3498ec8416b07a1787fec558d2bd910e
skris92 <szurcsik92@gmail.com>
2022-03-26 21:18:48 +0100

    Fully functioning Act method

--- src/Codecool.LifeOfAnts/Worker.cs
+++ src/Codecool.LifeOfAnts/Worker.cs
@@ -14,8 +14,60 @@ namespace Codecool.LifeOfAnts
         }
         public override void Act(Ant[,] area)
         {
+            List<Position> AntPositions = GetAntPositionsList(area);
+            Position NextPosition = GenerateNextPosition(area);
+            if (!IsPositionOccupied(AntPositions, NextPosition))
+            {
+                Position = new Position(NextPosition.X, NextPosition.Y);
+            }
+        }
+        private Position GenerateNextPosition(Ant[,] area)
+        {
             Random random = new Random();
-            Direction randomDrection = (Direction)random.Next(0, 5);
+            Direction direction = (Direction)random.Next(0, 4);
+
+            if (direction == Direction.North && this.Position.X > 0)
+            {
+                return new Position(this.Position.X - 1, this.Position.Y);
+            }
+            else if (direction == Direction.South && this.Position.X < area.GetLength(1) - 1)
+            {
+                return new Position(this.Position.X + 1, this.Position.Y);
+            }
+            else if (direction == Direction.West && this.Position.Y > 0)
+            {
+                return new Position(this.Position.X, this.Position.Y - 1);
+            }
+            else if (direction == Direction.East && this.Position.Y < area.GetLength(0) - 1)
+            {
+                return new Position(this.Position.X, this.Position.Y + 1);
+            }
+            else
+            {
+                return new Position(this.Position.X, this.Position.Y);
+            }
+        }
+        private bool IsPositionOccupied(List<Position> AntPositions, Position NextPosition)
+        {
+            bool isOccupied = false;
+            foreach (Position antPositon in AntPositions)
+            {
+                if (antPositon.X == NextPosition.X && antPositon.Y == NextPosition.Y)
+                {
+                    isOccupied = true;
+                    break;
+                }
+            }
+            return isOccupied;
+        }
+        private List<Position> GetAntPositionsList(Ant[,] area)
+        {
+            List<Position> positions = new();
+            foreach(Ant ant in area)
+            {
+                if (ant != null) positions.Add(new Position(ant.Position.X, ant.Position.Y));
+            }
+            return positions;
         }
     }
 }
